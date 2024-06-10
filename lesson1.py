2e51178a3190c4d27ad42805f8d86387818b8d64
DavitAloev1 <davitalov28@gamil.com>
2024-06-10 21:05:38 +0400

    first commit

---
+++ lesson1.py
@@ -0,0 +1,41 @@
+from turtle import*
+to_the_point = " i am " #i am naming  parents , my , hobby ,data for a joke it isnt madee by ai or smth it is kinda funny nye heh heh heh
+name = "David "
+last_name = "Aloev "
+to_the_point_1 = "i am "
+age = "13 "
+to_the_point_2 = "years old."
+hobby_data = "about hobby.my hobby is practicing on myself XD reading books about proggraming."
+parents_data = "my parents name is Nato and Tazo their last name is Gioshvili and ALoevi "
+parents_age_data = "my mom Nato is 44 years old and my father Tazo is 40 years old"
+city_data = "i live in tbilisi  thats all  "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print( to_the_point + " " + name + " " + last_name + to_the_point_1 + age + to_the_point_2 + hobby_data + parents_data + parents_age_data + city_data)
