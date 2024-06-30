0241a36e4bf030a9a0f8635a05520d30b3ec8350
DavitAloev1 <davitalov28@gamil.com>
2024-06-30 15:55:30 +0400

    second commit

---
+++ level2/homework.py
@@ -0,0 +1,10 @@
+print("hello there i knew u are watching this at github its just dialogue")
+first =input("hi its lancy on the line are u busy?(yes/no)")
+if first == "no" :
+   print("ok were waiting for youat 9 o clock we will see you")
+elif first == "yes" :
+  print("ok goodbye")
+second=input("8*9=")
+if second == "72" :
+  print("great job")
+else :print("wrong")
